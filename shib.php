ab0fae18b6bd0bd3c7cf5a0fa6de176143cffeed
Terry Fleury <tfleury@illinois.edu>
2010-02-25 23:48:35 +0000

    Added new function to get a list of all InCommon IdPs.  This can be utilized to populate a dropdown list of (non-whitelisted) IdPs from which a user can select when requesting the school be added to our WAYF's whitelist.

--- shib.php
+++ shib.php
@@ -1,7 +1,12 @@
 <?php
 
+require_once("whitelist.php");
+
 /* Be sure to declare "global $shibarray" when using it in other files. */
 $shibarray = array();
+
+/* The full file path location of the local InCommon metadata XML file. */
+define('INCOMMON_METADATA','/etc/shibboleth/InCommon-metadata.xml');
 
 /************************************************************************
  * Function   : getShibInfo                                             *
@@ -27,7 +32,7 @@ $shibarray = array();
  * attributes needed for the cilogon.org service are present, then      *
  * the function returns true.                                           *
  ************************************************************************/
-function getShibInfo($metadata='/etc/shibboleth/InCommon-metadata.xml')
+function getShibInfo($metadata=INCOMMON_METADATA)
 {
     $retval = false;  // Assume not all necessary shib attributes are set
 
@@ -132,6 +137,118 @@ function getShibInfo($metadata='/etc/shibboleth/InCommon-metadata.xml')
     }
 
     return $retval;
+}
+
+/************************************************************************
+ * Function  : getInCommonIdPs                                          *
+ * Parameters: (1) The full path of the (InCommon) metadata XML file.   *
+ *                 If you do not specify this parameter or set this     *
+ *                 parameter to 'null', then the local InCommon         *
+ *                 metadata file will be used.  (Setting to 'null' is   *
+ *                 useful for setting the second parameter to the       *
+ *                 empty string while still using the default value     *
+ *                 for the first parameter).                            *
+ *             (2) The full path of the local Discovery Service         *
+ *                 whitelist.xml file.  The entityIDs in this file are  *
+ *                 omitted from the returned list of IdPs.  If set to   *
+ *                 the empty string '', then no entityIDs are omitted   *
+ *                 from the returned list of IdPs.                      *
+ * Returns   : An array of IdPs from a metadata file, typically the     *
+ *             InCommon metadata.  The keys of the array are the        *
+ *             entityIDs, and the values of the array are the "pretty   *
+ *             print" names of the IdPs.                                *
+ * This function scans a metadata file (defaults to the local InCommon  *
+ * metadata XML file) for IdPs, tagged as <IDPSSODescriptor>.  It       *
+ * finds the entityIDs for each such tag, as well as the "pretty print" *
+ * name of the IdP, stored in the <OrganizationDisplayName> tag.  The   *
+ * keys of the returned array are the entityIDs (for example            *
+ * "urn:mace:incommon:uiuc.edu"), while the values of the array are     *
+ * the display names (for example "University of Illinois at            *
+ * Urbana-Champaign").  The array is sorted alphabetically by the       *
+ * values (display names).  By default, the local Discovery Service's   *
+ * whitelist is read in as well.  The whitelist contains entityIDs that *
+ * appear in the local WAYF.  By default, this function OMITS the       *
+ * whitelisted IdPs from the returned array.  If you want a full list   *
+ * of IdPs (including the whitelisted IdPs), the pass 'null' as the     *
+ * first argument and an empty string as the second argument.           *
+ *                                                                      *
+ * The main purpose of this function is to get a list of InCommon       *
+ * IdPs which have not yet been added to the whitelist for the local    *
+ * WAYF.  This array can be used to populate a dropdown list of         *
+ * "potential" IdPs to be added to the white list.                      *
+ ************************************************************************/
+
+function getInCommonIdPs($metadata=INCOMMON_METADATA,
+                         $remove_whitelist=whitelist::defaultFilename)
+{
+    $idps = array();
+    $white = null;
+
+    /* By default, the whitelist for the local WAYF is read in *
+     * so as to remove IdPs which have already been accepted   *
+     * as releasing attributes to the SP.  If the second       *
+     * parameter is the empty string, then don't read in the   *
+     * local WAYF's whitelist file.                            */
+    if (strlen($remove_whitelist) > 0) {
+        $white = new whitelist($remove_whitelist);
+        $white->read();
+    }
+
+    /* Next, attempt to read in the specified metadata file    *
+     * create an XPATH query.  If the metadata file parameter  *
+     * is 'null', then set it to the default InCommon metadata *
+     * XML file.  This is useful for setting the whitelist     *
+     * file parameter to the empty string while still using    *
+     * the default InCommon metadata file.                     */
+    if (is_null($metadata)) {
+        $metadata = INCOMMON_METADATA;
+    }
+
+    if (is_readable($metadata)) {
+        $xmlstr = @file_get_contents($metadata);
+        if (strlen($xmlstr) > 0) {
+            $xmlstr = str_replace('xmlns=','ns=',$xmlstr);
+            $xml = new SimpleXMLElement($xmlstr);
+
+            /* This XPATH query is actually two queries in one. *
+             * The first half of the expression finds all       *
+             * entityIDs that contain <IDPSSODescriptor> tags   *
+             * (meaning they can act as an IDP).  The second    *
+             * half of the expression finds the corresponding   *
+             * <OrganizationDisplayName> for the entityIDs.     *
+             * This query is faster than doing an XPATH query   *
+             * to find the OrganizationDisplayName for each     *
+             * entityID, but ends up with an array that has     *
+             * entityIDs as the odd numbered elements and       *
+             * display names as the even numbered elements.     */
+            $result = $xml->xpath(
+                "//EntityDescriptor/IDPSSODescriptor" .
+                "/ancestor::EntityDescriptor" .
+                "/attribute::entityID" .
+                " | " .
+                "//EntityDescriptor/IDPSSODescriptor" .
+                "/ancestor::EntityDescriptor" .
+                "/Organization/OrganizationDisplayName"
+                );
+
+            /* Loop through the resulting array skipping any    *
+             * whitelisted IdPs if necessary.  Notice that the  *
+             * entityIDs are in the odd numbered array elements *
+             * and the display names are in the even numbered   *
+             * elements, so increment through the array by 2s.  */
+            for ($i = 0; $i < count($result); $i += 2) {
+                $entityID = (string)$result[$i]->entityID;
+                if (($white == null) || (!$white->exists($entityID))) {
+                    $idps[$entityID] = (string)$result[$i+1];
+                }
+            }
+
+            /* Finally sort the array by the display names.     */
+            natcasesort($idps);
+        }
+    }
+
+    return $idps;
 }
 
 /************************************************************************

651e0d185d9c3477458df64f0dce9dcaee36c3b3
Terry Fleury <tfleury@illinois.edu>
2010-06-03 19:52:26 +0000

    Fixed warning message from Yahoo OpenID using local cilogon.xrds file.

--- content.php
+++ content.php
@@ -39,6 +39,8 @@ function printHeader($title='',$extra='')
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head><title>' . $title . '</title> 
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-XRDS-Location" 
+          content="https://cilogon.org/cilogon.xrds"/>
     <link rel="stylesheet" type="text/css" href="/include/cilogon.css" />
     <script type="text/javascript" src="/include/secutil.js"></script>
     ';

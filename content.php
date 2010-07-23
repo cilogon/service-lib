b27020c25b9f6c9fa6707a259dc80a46f36f6ebd
Terry Fleury <tfleury@illinois.edu>
2010-07-23 22:03:46 +0000

    Commented out the "next service outage" message.

--- content.php
+++ content.php
@@ -119,13 +119,17 @@ function printFooter($footer='')
  * the four edges of the box and then outlines the inner box.           *
  ************************************************************************/
 function printPageHeader($text) {
+/*
     echo '
     <div class="noticebanner">
     The CILogon Service is in beta testing.
     Unscheduled service outages may occur.
     The next scheduled outage is <em>Friday, July 23</em>.
     </div>
+    ';
+*/
 
+    echo '
     <div class="t">
     <div class="b">
     <div class="l">

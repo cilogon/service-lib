4dd22b1f10d62975ea93b6ea31f3684955e587ce
Terry Fleury <tfleury@illinois.edu>
2011-03-07 21:07:07 +0000

    Make 'viewport' slightly smaller for Jim's iPhone.

--- content.php
+++ content.php
@@ -41,7 +41,7 @@ function printHeader($title='',$extra='')
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head><title>' , $title , '</title> 
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="initial-scale=0.8" />
+    <meta name="viewport" content="initial-scale=0.6" />
     <meta http-equiv="X-XRDS-Location" 
           content="https://' , HOSTNAME , '/cilogon.xrds"/>
     <link rel="stylesheet" type="text/css" href="/include/cilogon.css" />
